f4c228023056aaad07f1feec475a31016b544114
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-03-13 16:19:00 +0300

    Add project files.

---
+++ Week1/ForLoop-Arrays-Conditions/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = { 8, 14, 52, -8, 1, 3 };
+
+//Dizi Listelemesi 
+Console.Write("Dizi: ");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write(numbers[i] + " ");
+}
+Console.Write("\n");
+
+//Dizideki En Büyük Sayı
+int enBuyuk = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    enBuyuk = numbers[i] > enBuyuk ? numbers[i] : enBuyuk;
+}
+Console.WriteLine($"En Büyük Sayı: {enBuyuk}");
+
+//Dizideki En Küçük Sayı
+int enKucuk = numbers[0];
+for (int i = 0; i < numbers.Length; i++)
+{
+    if (numbers[i] < enKucuk)
+    {
+        enKucuk = numbers[i];
+    }
+}
+Console.WriteLine($"En Küçük Sayı: {enKucuk}");
+
+//Dizideki Elemanların Toplamı
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    sum += numbers[i];
+}
+Console.WriteLine($"Elemanların Toplamları: {sum}");
+
+//Dizideki Elemanların Ortalaması
+double avg = (double)sum / (double)numbers.Length;
+Console.WriteLine($"Elemanların Ortalaması: {avg}");
+
+//Leetcode Search Insert Position Algoritma Çözümü
+int[] nums = { 1, 3, 5, 6 };
+int target = 7, u = 0;
+for (; u < nums.Length; u++)
+{
+    if (nums[u] == target)
+    {
+        break;
+    }
+    else
+    {
+        if (nums[u] > target)
+        {
+            break;
+        }
+    }
+}
+Console.WriteLine(u);
+
---
+++ Week1/WhileLoop-Array-Conditions/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] numbers = { 13, 8, -9, 52, 4, 5 };
+
+//Dizi Listelemesi 
+int index = 0;
+Console.Write("Dizi: ");
+while (index < numbers.Length)
+{
+    Console.Write(numbers[index++] + " ");
+}
+Console.Write("\n");
+
+//Dizideki En Büyük Eleman
+int enBuyuk = numbers[0], i = 0;
+while (i < numbers.Length)
+{
+    if (enBuyuk < numbers[i])
+    {
+        enBuyuk = numbers[i];
+    }
+    i++;
+}
+Console.WriteLine($"En Büyük Sayı: {enBuyuk}");
+
+//Dizideki En Küçük Eleman
+int enKucuk = numbers[0], j = 0;
+while (j < numbers.Length)
+{
+    if (numbers[j] < enKucuk)
+    {
+        enKucuk = numbers[j];
+    }
+    j++;
+}
+Console.WriteLine($"En Küçük Sayı: {enKucuk}");
+
+//Dizideki Elemanların Toplamı
+int k = 0, sum = 0;
+while (k < numbers.Length)
+{
+    sum += numbers[k++];
+}
+Console.WriteLine($"Toplamları: {sum}");
+
+//Ortalamaları
+double avg = (double)sum / (double)numbers.Length;
+Console.WriteLine($"Ortalamaları: {avg}");
+
