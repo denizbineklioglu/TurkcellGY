86ec460f4ff25c8363750532fb60d24668c0a5cf
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-03-15 13:56:49 +0300

    password control

---
+++ Week2/PasswordControl/Program.cs
@@ -0,0 +1,151 @@
+﻿namespace PasswordControl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                1. En az 6 karakter
+             *  2. Sadece harf ya da sadece sayı ise ZAYIF ya da sadece alfanümerik olmayan ise
+             *  3. Hem harf hem sayı ise ORTA
+             *  4. Hem sayı, hem harf hem de alfanümerik olmayan bir karakter varsa GÜÇLÜ şifre desin.
+             *  
+             *  İpucu: 
+             *  char(.)
+            */
+
+            Console.Write("Bir şifre giriniz: ");
+            string password = Console.ReadLine();
+
+            if (IsPassword6Character(password))
+            {
+                if (IsLetter(password) || IsDigit(password))
+                {
+                    Console.WriteLine("ZAYIF şifre");
+                }
+                else
+                {
+                    if (!IsLetterAndDigit(password))
+                    {
+                        if (IsLetterAndDigitAndAlfaNumeric(password))
+                        {
+                            Console.WriteLine("GÜÇLÜ şifre");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("ORTA şifre.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Şifre 6 karakterden az olamaz!");
+            }
+
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Söz diziminin içerisinde sayısal
+        /// bir değer olup olmadığını kontrol eder.
+        /// </summary>
+        /// <param name="password">Söz Dizimi</param>
+        /// <returns></returns>
+        public static bool IsDigit(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Söz diziminin sadece karakterlerden mi oluştuğunu
+        /// kontrol eder. 
+        /// </summary>
+        /// <param name="password">Söz Dizimi
+        /// </param>
+        /// <returns></returns>
+        public static bool IsLetter(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsLetter(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Söz dizimi içerisinde hem karakter hem de 
+        /// sayısal karakterler olup olmadığını kontrol eder. 
+        /// </summary>
+        /// <param name="password">Söz Dizimi</param>
+        /// <returns></returns>
+        public static bool IsLetterAndDigit(string password)
+        {
+            foreach (char c in password)
+            {
+                if (!char.IsLetterOrDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// Söz diziminin 6 karakterden uzun mu 
+        /// kısa mı olduğunu kontrol eder.
+        /// </summary>
+        /// <param name="password">Söz dizimi</param>
+        /// <returns></returns>
+        public static bool IsPassword6Character(string password)
+        {
+            if (password.Length < 6)
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        /// <summary>
+        /// Söz diziminin içerisinde hem numeric
+        /// hem alfa numeric hem de normal karakterlerden olup olmadığını
+        /// kontrol eder. 
+        /// </summary>
+        /// <param name="password">Söz Dizimi</param>
+        /// <returns></returns>
+        public static bool IsLetterAndDigitAndAlfaNumeric(string password)
+        {
+            bool letter = false, digit = false, symbol = false;
+
+            foreach (char c in password)
+            {
+                if (char.IsLetter(c))
+                {
+                    letter = true;
+                }
+                else if (char.IsDigit(c))
+                {
+                    digit = true;
+                }
+                else
+                {
+                    symbol = true;
+                }
+            }
+            return letter & digit & symbol;
+        }
+    }
+}
