24a75ea651a883e003fe3d94b00ca80020cab304
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-03-17 22:10:04 +0300

    oop exercises

---
+++ Week2/OOPExercises/Chef-Food-Presentation/Chef.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPExercises.Chef_Food_Presentation
+{
+    public class Chef
+    {
+
+        public void Cook(Food food)
+        {
+            food.MakePresentation();
+        }
+
+    }
+}
---
+++ Week2/OOPExercises/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+//Virtual - Override
+using OOPExercises.Chef_Food_Presentation;
+
+KuruFasulye krf = new KuruFasulye() { Name = "Kuru Fasulye" };
+Baklava baklava = new Baklava() { Name = "Baklava" };
+Chef chef = new Chef();
+
+chef.Cook(krf);
+chef.Cook(baklava);
+
+
+
+
+
+
