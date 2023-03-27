5055875c9a5a01446f74f7bab102199ed6f71754
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-03-28 02:49:41 +0300

    dependency inversion

---
+++ SolidPrinciples/5- DIP/Exercise/IGeneric.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+
+    public class CustomerManager
+    {
+        private readonly IDatabase _database;
+
+        public CustomerManager(IDatabase database)
+        {
+            this._database = database;
+        }
+
+        public void Add(Customer customer)
+        {
+            _database.Insert(customer);
+        }
+
+        public void Delete(int id)
+        {
+            _database.Delete(id);
+        }
+
+        public void Update(Customer customer)
+        {
+            _database.Update(customer);
+        }
+
+    }
+
+
+    public interface IGenericDal<T>
+    {
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+
+
+    public interface IDatabase: IGenericDal<Customer>
+    {
+
+    }
+
+    //Sql Server Kullanarak.
+    public class SqlCustomerDal : IDatabase
+    {
+        public void Delete(int id)
+        {
+            Console.WriteLine("deleted with sql server");
+        }
+
+        public void Insert(Customer entity)
+        {
+            Console.WriteLine("inserted with sql server");
+        }
+
+        public void Update(Customer entity)
+        {
+            Console.WriteLine("updatd with sql server");
+        }
+    }
+
+
+    //Oracle Kullanarak.
+    public class OracleCustomerDal : IDatabase
+    {
+        public void Delete(int id)
+        {
+            Console.WriteLine("deleted with oracle");
+        }
+
+        public void Insert(Customer entity)
+        {
+            Console.WriteLine("inserted with oracle");
+        }
+
+        public void Update(Customer entity)
+        {
+            Console.WriteLine("updated with oracle");
+        }
+    }
+
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+
+}
---
+++ SolidPrinciples/5- DIP/Exercise/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise;
+
+Console.WriteLine("Hello, World!");
+
+
+CustomerManager cm = new CustomerManager(new SqlCustomerDal());
+CustomerManager cm2 = new CustomerManager(new OracleCustomerDal());
+
+cm.Add(new Customer { ID =1 , Name = "Deniz"});
+cm.Delete(1);
+
+cm2.Add(new Customer { ID = 2, Name = "Cansu" });
+cm2.Delete(2);
