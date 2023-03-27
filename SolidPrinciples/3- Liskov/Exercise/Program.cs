fda8704a5f431c034bf8664bc3804cde2bc9bf53
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-03-27 23:35:43 +0300

    liskov

---
+++ SolidPrinciples/3- Liskov/Exercise/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercise;
+
+
+/*
+    Liskov's Substitution principle
+    Bir sınıf, miras aldığı sınıfın özelliklerini aynen kullanmak zorundadır. 
+    Bir sınıf, bir başkasına miras veriyorsa bu sınıflar birbirleri yerine kullanılabilmelidirler.
+   
+ */
+
+
+
+var rent = Rents.Validate(DateTime.Now, 15, 2);
+var rent1 = Rents.Validate(DateTime.Now, 15,model:"Supra");
+var rent2 = Rents.Validate(DateTime.Now, 15);
+
+rent.Validation();
+rent1.Validation();
+rent2.Validation(); 
---
+++ SolidPrinciples/3- Liskov/Exercise/Rents.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public static class Rents
+    {
+        public static IRentable Validate(DateTime date, int rezervationDayCount, int? personCount = null, string model = null)
+        {
+            if (personCount.HasValue)
+            {
+                return new Meeting() { Date = date,RezervationDayCount = rezervationDayCount,PersonCount = personCount};
+            }
+            else if (model != null)
+            {
+                return new Car() { Date = date,RezervationDayCount = rezervationDayCount, Model = model};
+            }
+            else
+            {
+                return new Otel() { Date = date, RezervationDayCount = rezervationDayCount };
+            }
+        }
+    }
+
+
+    public interface IRentable
+    {
+        void Validation();
+    }
+
+    public class Entity
+    {
+        public DateTime Date { get; set; }
+        public int RezervationDayCount { get; set; }
+    }
+
+
+    public class Car : Entity, IRentable
+    {
+        public string Model { get; set; }
+        public void Validation()
+        {
+            Console.WriteLine("Car rented.");
+        }
+    }
+
+    public class Otel : Entity, IRentable
+    {        
+        public void Validation()
+        {
+            Console.WriteLine("Hotel Rented");
+        }
+    }
+
+    public class Meeting : Entity, IRentable
+    {
+        public int? PersonCount { get; set; }
+        public void Validation()
+        {
+            Console.WriteLine("Meeting room rented.");
+        }
+    }
+}
