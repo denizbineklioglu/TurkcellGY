9338ca169bc8199644372adc6b1c8403819654ea
Deniz Bineklioğlu <76698070+denizbineklioglu@users.noreply.github.com>
2023-03-28 03:03:46 +0300

    solid finish

---
+++ SolidPrinciples/4- ISP/Exercise/IPayment.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public interface IPayment
+    {
+        void GetPayments();
+        void GetPaymentStatus();
+    }
+
+
+    public class BankPayment : IPayment
+    {
+        public void GetPayments()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetPaymentStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    /*
+        Bu prensib gereği,implement edilen
+        interface'deki tüm özellikler kullanılmak zorunda.
+        Kullanılmayan 1 tane bile özellik olmamalı.
+     */
+
+    public class ATMPayment //:IPayment
+    {
+        public void GetPayments()
+        {
+            throw new NotImplementedException();
+        }
+
+        //public void GetPaymentStatus()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+
+
+
+
+}
